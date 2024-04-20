7bf2ae7de2240794c53a99ecc611436a50a637f0
Qi Wang <qi_wang_zz@outlook.com>
2024-04-20 23:06:52 +0800

    Update auto update exist repo

--- MirrorListRepositories.ps1
+++ MirrorListRepositories.ps1
@@ -5,10 +5,12 @@ param (
 
 $URLList = @(
     "https://github.com/ZhiZe-ZG/zhize-zg.github.io",
-    "https://github.com/ZhiZe-ZG/GitMirrorManagement.git",
+    "https://github.com/ZhiZe-ZG/GitMirrorManagement",
     "https://github.com/ZhiZe-ZG/FFmpegCompressionScript",
     "https://github.com/ZhiZe-ZG/sin-activator-paper"
 )
+
+$FailedList = @()
 
 # Clone all the repositories
 $URLList | ForEach-Object {
@@ -19,6 +21,39 @@ $URLList | ForEach-Object {
         $repoName += ".git"
     }
     $localPath = Join-Path -Path $targetPath -ChildPath $repoName
-    git clone --mirror $_ $localPath
+    # check if the localPath exists
+    if (Test-Path $localPath) {
+        Write-Output "$repoName already exists, now updating"
+        $originalPath = Get-Location
+        Set-Location -Path $localPath
+        git fetch --all
+        Set-Location -Path $originalPath
+    }
+    else {
+        try {
+            Write-Output "Cloning $repoName to $localPath"
+            git clone --mirror $_ $localPath
+            if ($LastExitCode -ne 0) {
+                throw "Command failed with exit code $LastExitCode"
+            }
+        }
+        catch {
+            # add the URL to FailedList
+            $FailedList += $repoName
+        }
+    }
 }
 
+# Show all failed
+# check if the FailedList is empty
+if ($FailedList.Count -eq 0) {
+    Write-Host -ForegroundColor Green "All repositories are cloned successfully" 
+    exit 0
+}
+else {
+    Write-Host -ForegroundColor Red "Failed to clone the following repositories:" 
+    $FailedList | ForEach-Object {
+        Write-Output $_
+    }
+    exit 1
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # GitMirrorManagement
+
 Some PowerShell to manage mirroring and backups of my git repository.
