b891ba0894e1b4f93b07f5f57ab49a700fe45c1c
Qi Wang <qi_wang_zz@outlook.com>
2024-04-23 14:48:02 +0800

    add comment grammar

--- ConfigurationFileOperations.ps1
+++ ConfigurationFileOperations.ps1
@@ -4,7 +4,7 @@ function Read-MirrorListFile {
     )
     # Read the list of repositories from the file
     # Each line of this file is a URL of a repository
-    # ignore blank lines
-    $urlList = Get-Content -Path $mirrorListFilePath | Where-Object { $_ -match '\S' }
+    # ignore blank lines and lines start with `#`
+    $urlList = Get-Content -Path $mirrorListFilePath | Where-Object { $_ -match '\S' } | Where-Object { $_ -notmatch '^#' }
     return $urlList
 }
